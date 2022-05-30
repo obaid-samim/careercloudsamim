98dc750e85a111602fa7d2d43eb0fdd46cb2cffe
Obaid SAMIM <98791899+obaidsamim8@users.noreply.github.com>
2022-05-30 14:28:02 -0400

    Assignment 5 completed Humber

---
+++ CareerCloud.Pocos/ApplicantProfilePoco.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Applicant_Profiles")]
+    public class ApplicantProfilePoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [ForeignKey("FK_Applicant_Profiles_Security_Logins")]
+        public Guid Login { get; set; }
+        [Column("Current_Salary")]
+        public Decimal? CurrentSalary { get; set; }
+        [Column("Current_Rate")]
+        public Decimal? CurrentRate { get; set; }
+        public String Currency { get; set; }
+        [Column("Country_Code")]
+        [ForeignKey("FK_Applicant_Profiles_System_Country_Codes")]
+        public String Country { get; set; }
+        [Column("State_Province_Code")]
+        public String Province { get; set; }
+        [Column("Street_Address")]
+        public String Street { get; set; }
+        [Column("City_Town")]
+        public String City { get; set; }
+        [Column("Zip_Postal_Code")]
+        public String PostalCode { get; set; }
+        [Column("Time_Stamp")]
+        [NotMapped]
+        public Byte[] TimeStamp { get; set; }
+
+        public virtual SecurityLoginPoco SecurityLogin { get; set; }
+        public virtual SystemCountryCodePoco SystemCountryCode { get; set; }
+        public virtual ICollection<ApplicantEducationPoco> ApplicantEducations { get; set; }
+        public virtual ICollection<ApplicantJobApplicationPoco> ApplicantJobApplications { get; set; }
+        public virtual ICollection<ApplicantResumePoco> ApplicantResumes { get; set; }
+        public virtual ICollection<ApplicantSkillPoco> ApplicantSkills { get; set; }
+        public virtual ICollection<ApplicantWorkHistoryPoco> ApplicantWorkHistories { get; set; }
+    }
+}
